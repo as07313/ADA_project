4b3327294f113a2787fcdc10c9a9482d2623794e
ayilaemad <ae07352@st.habib.edu.pk>
2024-05-01 22:22:32 +0500

    Update dynamic_program.py

--- dynamic_program.py
+++ dynamic_program.py
@@ -14,6 +14,7 @@ def min_edit_distance_dp(s1, s2):
     for i in range(len_s1 + 1):
         dp[i][0] = i
         ops[i][0] = "delete"
+
     for j in range(len_s2 + 1):
         dp[0][j] = j
         ops[0][j] = "insert"
@@ -21,16 +22,31 @@ def min_edit_distance_dp(s1, s2):
     # Fill in the table
     for i in range(1, len_s1 + 1):
         for j in range(1, len_s2 + 1):
+
+            # To delete a character from s1, the cost is 1 plus the minimum edit distance
+            # for the substring s1[:i-1] and the full substring s2 up to position j.
             delete = dp[i - 1][j] + 1
+
+            # To insert a character into s1, the cost is 1 plus the minimum edit distance
+            # for the full substring s1 up to position i and the substring s2[:j-1].
             insert = dp[i][j - 1] + 1
+
+            # If the characters at positions i and j in s1 and s2 are the same,
+            # no operation is needed, so the cost is the minimum edit distance
+            # for the substrings s1[:i-1] and s2[:j-1].
             match = dp[i - 1][j - 1]
+
+            # If the characters at positions i and j in s1 and s2 are different,
+            # the cost is 1 plus the minimum edit distance for the substrings
+            # s1[:i-1] and s2[:j-1], as one substitution operation is required.
             mismatch = dp[i - 1][j - 1] + 1
+
             if s1[i - 1] == s2[j - 1]:
                 dp[i][j] = match
                 ops[i][j] = "match"
-
             else:
                 dp[i][j] = min(delete, insert, mismatch)
+
                 if dp[i][j] == delete:
                     ops[i][j] = "delete"
                 elif dp[i][j] == insert:
