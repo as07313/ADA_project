e2e0568622d7cd269fc53712268c0b9ac256198f
ayilaemad <ae07352@st.habib.edu.pk>
2024-05-01 22:22:08 +0500

    Update dynamic_program_memo.py

--- dynamic_program_memo.py
+++ dynamic_program_memo.py
@@ -1,11 +1,10 @@
 '''
 Minimum edit distance using dynamic programming with memoization
 '''
+# memoization dictionary
+memo = {}
 
-def min_edit_distance_memo(s1, s2):
-
-    # memoization dictionary
-    memo = {}    
+def min_edit_distance_memo(s1, s2, memo):
     
     n = len(s1)
     m = len(s2)
@@ -22,13 +21,13 @@ def min_edit_distance_memo(s1, s2):
     
     # if the last characters are the same, we don't need to do anything move to the next characters
     if s1[n-1] == s2[m-1]:
-        return min_edit_distance_memo(s1[:n-1], s2[:m-1])
+        return min_edit_distance_memo(s1[:n-1], s2[:m-1],memo)
     
 
     # if the last characters are different, we have three options:
-    memo[(n, m)] = 1 + min(min_edit_distance_memo(s1[:n-1], s2),
-                            min_edit_distance_memo(s1, s2[:m-1]), 
-                            min_edit_distance_memo(s1[:n-1], s2[:m-1]))
+    memo[(n, m)] = 1 + min(min_edit_distance_memo(s1[:n-1], s2,memo),
+                            min_edit_distance_memo(s1, s2[:m-1],memo), 
+                            min_edit_distance_memo(s1[:n-1], s2[:m-1],memo))
     
     return memo[(n, m)]
 
